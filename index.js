b647e1191bcb0b8d8973ed920b22fe75a4c128d4
seekopath <69005667+seekopath@users.noreply.github.com>
2021-07-11 18:16:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,6 +17,14 @@ app.get("/", (req, res) => {
   res.send("Running");
 });
 
+if(process.env.NODE_ENV=="production"){
+    app.use(express.static('client/build'))
+    const path = require('path')
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    })
+}
+
 io.on("connection", (socket) => {
   socket.emit("me", socket.id);
 
